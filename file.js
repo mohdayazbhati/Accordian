68e81d50110feebbe0342b1b15bc3b2005c99eb0
chhatrapati295 <116446768+chhatrapati295@users.noreply.github.com>
2022-11-17 22:01:42 +0530

    change 3

--- file.js
+++ file.js
@@ -1,24 +1,4 @@
-// const ourItemDiv = document.getElementsByClassName("item");
-// const openIcon = document.getElementsByClassName("iconOpen");
-// const closeIcon = document.getElementsByClassName("iconClose");
 
-// // console.log(ourItemDiv);
-
-// for (let i = 0; i < ourItemDiv.length; i++) {
-//   closeIcon[i].style.display = "none";
-
-//   ourItemDiv[i].addEventListener("click", () => {
-//     const result = ourItemDiv[i].classList.toggle("active");
-
-//     if (result) {
-//       closeIcon[i].style.display = "block";
-//       openIcon[i].style.display = "none";
-//     } else {
-//       closeIcon[i].style.display = "none";
-//       openIcon[i].style.display = "block";
-//     }
-//   });
-// }
 let itemdiv = document.getElementsByClassName('item')
 let iconClose = document.getElementsByClassName('iconClose')
 let iconOpen = document.getElementsByClassName('iconOpen')
